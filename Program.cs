11b599120a881ddd3f03403431ebabeb3f69b4b3
Amanda Santos <98029661+Asantosvp@users.noreply.github.com>
2024-01-03 12:46:25 -0300

    including arithmetic operators

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using calculadora_com_C_.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10,30);
+calc.Subtrair(10,40);
+calc.Multiplicar(10,50);
+calc.Dividir(10,2);
