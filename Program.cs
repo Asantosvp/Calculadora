e4813b64c2c472ea514b5aaa2a1a7ab2bc9afbdd
Amanda Santos <98029661+Asantosvp@users.noreply.github.com>
2024-01-03 13:21:51 -0300

    using power

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 
 Calculadora calc = new Calculadora();
 
-calc.Somar(10,30);
-calc.Subtrair(10,40);
-calc.Multiplicar(10,50);
-calc.Dividir(10,2);
+// calc.Somar(10,30);
+// calc.Subtrair(10,40);
+// calc.Multiplicar(10,50);
+// calc.Dividir(10,2);
+
+calc.Potencia(3, 3);
